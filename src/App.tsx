b28439cd7dbf33029a11a51de1f97830e2ab6464
Sapnil Aditya Ayman <sapnilaa@ulrik.uio.no>
2025-08-06 01:03:23 +0200

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import HomePage from './pages/home/HomePage';
 
 function App() {
   return (
-    <></>
+    <Routes>
+      <Route path = "/" element = {<HomePage />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,11 @@
+
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Nettside</h1>
+        </div>
+    )
+}
+
+export default HomePage
