3dfae2a90b47e8aa61aeeab8947d5ac1e10ebc50
Sapnil Aditya Ayman <sapnilaa@ulrik.uio.no>
2025-08-09 17:56:16 +0200

    Add wallpaper and folder for images

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import HomePage from './pages/home/HomePage';
 import TopBar from './components/TopBar/TopBar';
+import wallpaper from './assets/images/one_piece_wallpaper.jpg'
 
 function App() {
   return (
     <div>
       <TopBar/>
-      <div className = "pt-16">
+      <div className = "pt-8">
+        <img className = "w-screen h-screen" src={wallpaper} alt = "wallpaper" />
         <Routes>
           <Route path = "/" element = {<HomePage />} />
         </Routes>
