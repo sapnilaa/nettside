d679069db5e27859192a7af61f7a0acb42649f0b
Sapnil Aditya Ayman <sapnilaa@ulrik.uio.no>
2025-08-27 00:51:43 +0200

    Remove wallpaper and add json-file with cv-info and other stuff

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,14 @@
+
+
+
+
+function Modal() {
+    return (
+        <div className = "fixed h-[500px] w-[700px] flex items-center justify-center bg-green-700 bg-opacity-85 px-4 py-2 hover:bg-opacity-100">
+            <p className = "font-semibold">Halloisen!</p>
+        </div>
+    )
+    
+}
+
+export default Modal;
