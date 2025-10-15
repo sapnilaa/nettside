f91c0a285444ae39e630c61e19ab91e21d54affd
Sapnil Aditya Ayman <sapnilaa@ulrik.uio.no>
2025-10-15 22:03:19 +0200

    Fix issues related to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/home/HomePage';
 import TopBar from './components/TopBar/TopBar';
 import InfoPage from './pages/info/InfoPage';
 
 function App() {
   return (
-    <div className="ml-40 mr-40">
-      <TopBar/>
-      <div className="pt-8">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/info" element={<InfoPage />} />
-        </Routes>
+      <div className="ml-40 mr-40">
+          <TopBar/>
+          <div className="pt-8">
+              <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/info" element={<InfoPage />} />
+              </Routes>
+        </div>
       </div>
-    </div>
   );
 }
 
